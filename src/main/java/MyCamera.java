40173422da03c5c515d96ea5fad7067daa279982
LeCadavrExquis <filipzarnowiec@gmail.com>
2020-10-22 20:22:55 +0200

    MyCamera - constructor and grab and stop method

--- src/main/java/MyCamera.java
+++ src/main/java/MyCamera.java
@@ -1,8 +1,27 @@
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.opencv.opencv_core.Mat;
 
 public class MyCamera {
-    public Mat grabImage() {
-        //TODO implement grabImage
-        return null;
+    protected FrameGrabber grabber;
+    protected CanvasFrame canvasFrame;
+    protected OpenCVFrameConverter.ToMat converter;
+
+    public MyCamera() throws FrameGrabber.Exception {
+        this.grabber = FrameGrabber.createDefault(0);
+        grabber.start();
+        this.canvasFrame = new CanvasFrame("SomeTitle", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+        this.converter = new OpenCVFrameConverter.ToMat();
+    }
+
+
+    public Mat grabImage() throws FrameGrabber.Exception {
+        return converter.convert(grabber.grab());
+    }
+
+    public void stop() throws FrameGrabber.Exception {
+        canvasFrame.dispose();
+        grabber.stop();
     }
 }
