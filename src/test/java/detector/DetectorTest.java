2098627c267e25713dc59e8bb33c2297389a2013
LeCadavrExquis <filipzarnowiec@gmail.com>
2020-10-25 23:23:09 +0100

    Some images added for testing

---
+++ src/test/java/detector/DetectorTest.java
@@ -0,0 +1,18 @@
+package detector;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class DetectorTest {
+
+    @Test
+    void drawDetectedEyes() {
+        fail("not implelemnted");
+    }
+
+    @Test
+    void getIrisColor() {
+        fail("not implemented");
+    }
+}
